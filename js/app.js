7f04c53daa02ae253289f423b6a007faa656fc69
Abdubannob <abduhamidovm52@gmail.com>
2023-04-13 23:31:14 +0300

    first deploy

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+let screen = document.querySelector(".screen")
+let noti = document.getElementById("item-notificate")
+let body = document.getElementById('body')
+let gradi = document.querySelector(".linear-gradient")
+document.getElementById("notification-cancel").onclick = () => {
+    noti.style.display = "none"
+    gradi.style.display ="none"
+}
+
+document.getElementById("notification").onclick =()=>{
+    screen.style.display = "inline-block"
+    noti.style.display = "flex"
+    gradi.style.display ="inline-block"
+
+}
+
+document.getElementById("screen").onclick =()=>{
+    screen.style.display = "none"
+}
