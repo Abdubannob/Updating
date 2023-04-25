3d1a98b52413b94449c29e4c22f6fb2ccb2f18eb
Abdubannob <abduhamidovm52@gmail.com>
2023-04-25 23:54:50 +0300

    deploy again

--- js/multiple.js
+++ js/multiple.js
@@ -1,8 +1,19 @@
 
 let multiple = document.querySelector(".multiple")
 let selection = document.querySelector(".languages")
+// let searchItem = document.querySelector(".checking")
 
+let searchItem = document.getElementById("sel")
 selection.onclick = function (){
+    
+    multiple.classList.toggle("actived");
+    selection.classList.toggle("actived");
+
+
+}
+
+searchItem.onclick = function (){
+    
     multiple.classList.toggle("actived");
     selection.classList.toggle("actived");
 
