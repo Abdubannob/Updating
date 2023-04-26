29048e4c62c98157434ab93163e2853fa3bbc6dc
Abdubannob <abduhamidovm52@gmail.com>
2023-04-26 16:06:30 +0300

    deploy

--- js/multiple.js
+++ js/multiple.js
@@ -1,41 +1,120 @@
 
 let multiple = document.querySelector(".multiple")
 let selection = document.querySelector(".languages")
-let select = document.querySelector("selection-wrapper")
-let selectItems = document.querySelector("#select-one")
 
-let searchItem = document.getElementById("sel")
-selection.onclick = function (){
-    
-    multiple.classList.toggle("actived");
+let selectionCategory = document.querySelector(".languages-category")
+let selectionTwo = document.querySelector(".languages-two")
+let budgetBTN= document.querySelector(".btns")
+
+let select = document.querySelector(".selection-wrapper")
+let selectItems = document.querySelector(".selection-btn")
+let socialbtn = document.querySelector(".social-btn")
+
+let selectbudget = document.querySelector(".selection-wrapper-budget")
+let selectItemsBudget = document.querySelector(".selection-btn-budget")
+
+let LocationBtn = document.querySelector(".location-btn")
+let selectItemsLocation = document.querySelector(".selection-btn-location")
+let selectLocationWrapper = document.querySelector(".selection-wrapper-location")
+
+let DeliveryBtn = document.querySelector(".delivery-btn")
+let selectItemsDelivery = document.querySelector(".selection-btn-delivery")
+let selectItemsWrapper = document.querySelector(".selection-wrapper-delivery")
+
+let ser = document.querySelector(".search-item-multiple")
+let searchItem = document.querySelector(".operation-select")
+let searchItemTwo = document.getElementById("network")
+// let searchItemOrder = document.getElementById("SelTwo")
+selection.onclick = function (){  
     selection.classList.toggle("actived");
-
-
-}
-
-selectItems.onclick = function (){
     
-    selectItems.classList.toggle("activetion");
-   
 }
 
-
-searchItem.onclick = function (){
+multiple.onclick = function (){  
+    multiple.classList.add("actived");
     
-    multiple.classList.toggle("actived");
-    select.classList.toggle("actived");
-
-
 }
+
+selectionCategory.onclick = function (){  
+  
+    selectionCategory.classList.toggle("category-actived");
+}
+
+selectbudget.onclick = function (){
+    selectItemsBudget.classList.add("activetion-two")
+}
+
+multiple.onclick = function (){
+    selectionCategory.classList.toggle("category-actived");
+}
+
+socialbtn.onclick = function (){ 
+    selectItems.classList.toggle("activetion");  
+}
+
+select.onclick = function(){
+    selectItems.classList.add("activetion");  
+}
+
+selectItemsBudget.onclick = function (){ 
+    selectbudget.classList.add("activetion-two");  
+}
+budgetBTN.onclick = function (){ 
+    selectItemsBudget.classList.toggle("activetion-two");  
+}
+
+DeliveryBtn.onclick = function (){ 
+    selectItemsDelivery.classList.toggle("delivery-actived");  
+}
+
+selectItemsWrapper.onclick = function (){ 
+    selectItemsWrapper.classList.add("delivery-actived");  
+}
+selectLocationWrapper.onclick = function (){ 
+    selectItemsLocation.classList.add("activetion-two");  
+}
+LocationBtn.onclick = function (){ 
+    selectItemsLocation.classList.toggle("activetion-two");  
+}
+
+
+
+
+
+// searchItemTwo.onclick = function (){
+//     selectItems.classList.toggle("activetion");
+    
+// }
+
+
 
 document.onclick = function(e){
-    if(!multiple.contains(e.target) && !selection.contains(e.target)){
-        multiple.classList.remove("actived");
-        selection.classList.remove("actived");
-    }
-
-    if(!searchItem.contains(e.target) && !select.contains(e.target)){
+    
+    if(!selectItems.contains(e.target) && !select.contains(e.target)){
         selectItems.classList.remove('activetion')
     }
+    if( !selection.contains(e.target) && !multiple.contains(e.target)){
+        selection.classList.remove("actived");
+    }
+    if(!selectItemsBudget.contains(e.target) && !selectbudget.contains(e.target)){
+        selectItemsBudget.classList.remove('activetion-two')
+    }
+
+    if(!selectItemsLocation.contains(e.target) ){
+        selectItemsLocation.classList.remove('activetion-two')
+    }
     
+    if(!selectionCategory.contains(e.target) ){
+        selectionCategory.classList.remove('category-actived')
+    }
+
+    if(!selectItemsDelivery.contains(e.target) ){
+        selectItemsDelivery.classList.remove('delivery-actived')
+    }
+
+}
+
+searchItem.onclick = function (){
+    select.classList.toggle("actived");
+    selection.classList.toggle("actived")
 }
