c199f5274de87d3436b7ccb76ae118bafb3c9551
Abdubannob <abduhamidovm52@gmail.com>
2023-04-29 11:56:45 +0300

    deploy

--- js/ShowHide.js
+++ js/ShowHide.js
@@ -1,17 +1,13 @@
 
 let cancel = document.querySelector(".three-dots")
 let edit = document.querySelector(".three-dots-item")
-let notif = document.getElementById("notifcation")
-let body = document.querySelector("body")
+
 cancel.onclick = function (){
     cancel.classList.toggle("actives");
     edit.classList.toggle("actives");
 }
 
-notif.onclick = function () {
-    body.addClass("screen-gradient")
-    body.style.backgroundColor = "green"
-}
+
 
 document.onclick = function(e){
     if(!cancel.contains(e.target) && !edit.contains(e.target)){
--- js/notifcation.js
+++ js/notifcation.js
@@ -1,21 +1,22 @@
+
 let notif = document.getElementById("notifcation")
+let note = document.getElementById("notifcation-cancel")
 let body = document.querySelector("body")
-let bodies = document.querySelector(".note-contents")
-
-
-bodies.onclick = function () {
-    body.classList.add("screen-gradient")
-}
-
 
 notif.onclick = function () {
     body.classList.add("screen-gradient")
 }
 
-document.onclick = function(e){
-
-
-    if(!notif.contains(e.target) ){
-        body.classList.remove("screen-gradient");
-    }
+note.onclick = function () {
+    body.classList.remove("screen-gradient")
 }
+
+
+
+
+// document.onclick = function(e){
+//     if(!notif.contains(e.target) ){
+//         body.classList.remove("screen-gradient");
+//     }
+// }
+
