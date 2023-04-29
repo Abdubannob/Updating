1e48f641e4a2987338fda141272276573e6dc679
Abdubannob <abduhamidovm52@gmail.com>
2023-04-29 21:04:26 +0300

    deploy

--- js/notifcation.js
+++ js/notifcation.js
@@ -1,9 +1,21 @@
-
+let multiple = document.querySelector(".multiple")
+let selection = document.querySelector(".languages")
+let languagebtn = document.querySelector(".language-btn")
 let notif = document.getElementById("notifcation")
 let note = document.getElementById("notifcation-cancel")
-// let notes = document.getElementById("notifcation-cancel-nav")
+let notes = document.getElementById("notifcation-cancel-nav")
 let bod = document.querySelector(".notifcation-message")
 let notebody = document.querySelector(".note-contents")
+
+languagebtn.onclick = function (){  
+    selection.classList.toggle("category-actived");
+    
+}
+
+multiple.onclick = function (){  
+    multiple.classList.add("actived");
+    
+}
 
 notebody.onclick = function (){
     bod.classList.add("screen-gradient-two")
@@ -17,7 +29,13 @@ note.onclick = function () {
     bod.classList.remove("screen-gradient")
 }
 
+
 document.onclick = function(e){
+
+    if( !selection.contains(e.target) ){
+        selection.classList.remove("category-actived");
+    }
+
     if(!notif.contains(e.target) ){
         bod.classList.remove("screen-gradient");
     }
