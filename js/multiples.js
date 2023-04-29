f31367102157d7dd803bf1bbb0d03319ce3b7da1
Abdubannob <abduhamidovm52@gmail.com>
2023-04-29 21:26:44 +0300

    last changed

--- js/multiples.js
+++ js/multiples.js
@@ -141,3 +141,37 @@
 // //     select.classList.toggle("actived");
 // //     selection.classList.toggle("actived")
 // // }
+
+
+
+let notif = document.getElementById("notifcation")
+let note = document.getElementById("notifcation-cancel")
+let notes = document.getElementById("notifcation-cancel-nav")
+let bod = document.querySelector(".notifcation-message")
+let notebody = document.querySelector(".note-contents")
+
+
+notebody.onclick = function (){
+    bod.classList.add("screen-gradient-two")
+}
+
+notif.onclick = function () {
+    bod.classList.add("screen-gradient")
+}
+
+note.onclick = function () {
+    bod.classList.remove("screen-gradient")
+}
+
+
+document.onclick = function(e){
+
+  
+    if(!notif.contains(e.target) ){
+        bod.classList.remove("screen-gradient");
+    }
+    if(!notebody.contains(e.target)){
+        bod.classList.remove("screen-gradient-two");
+    }
+}
+
