3f51776df8cd9fcc8ef57b97d62b68ef58e2eb9f
Abdubannob <abduhamidovm52@gmail.com>
2023-04-29 18:36:16 +0300

    deploy

--- js/notifcation.js
+++ js/notifcation.js
@@ -1,22 +1,28 @@
 
 let notif = document.getElementById("notifcation")
 let note = document.getElementById("notifcation-cancel")
-let body = document.querySelector("body")
+// let notes = document.getElementById("notifcation-cancel-nav")
+let bod = document.querySelector(".notifcation-message")
+let notebody = document.querySelector(".note-contents")
+
+notebody.onclick = function (){
+    bod.classList.add("screen-gradient-two")
+}
 
 notif.onclick = function () {
-    body.classList.add("screen-gradient")
+    bod.classList.add("screen-gradient")
 }
 
 note.onclick = function () {
-    body.classList.remove("screen-gradient")
+    bod.classList.remove("screen-gradient")
 }
 
-
-
-
-// document.onclick = function(e){
-//     if(!notif.contains(e.target) ){
-//         body.classList.remove("screen-gradient");
-//     }
-// }
+document.onclick = function(e){
+    if(!notif.contains(e.target) ){
+        bod.classList.remove("screen-gradient");
+    }
+    if(!notebody.contains(e.target)){
+        bod.classList.remove("screen-gradient-two");
+    }
+}
 
